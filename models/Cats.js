ea06c6cd646baf0d2bba4eae44d8ac7eca3f3d53
keung0606 <keung123654@gmail.com>
2023-06-16 15:51:34 +0800

    Add Cat model schema

---
+++ models/Cats.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CatSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  breed: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female'],
+    required: true
+  }
+});
+
+const CatModel = mongoose.model("cats", CatSchema);
+module.exports = CatModel;
